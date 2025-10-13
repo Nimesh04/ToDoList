1c79f3d77239f67d8ee25a04bce5ebcee1598430
Nimesh04 <nimeshb907@gmail.com>
2025-10-13 13:27:45 -0500

    Redesigned Front-end
    All the elements and design completed for the
    front-end portion, need to connect the logic with
    back-end and complete the project.

--- src/index.js
+++ src/index.js
@@ -4,16 +4,53 @@ import { createProject } from "./createProjects.js";
 
 
 const projectBtn = document.querySelector("#createProj");
+const projectSection = document.querySelector(".projectSection");
+const taskBtn = document.querySelector("#add-task");
+const taskSection = document.querySelector(".taskSection");
+const projectCloseBtn = document.querySelector(".projectSection-heading > svg");
+const taskCloseBtn = document.querySelector(".taskSection-heading > svg");
+const titleForm = document.querySelector(".titleForm");
+const projectsTabDiv = document.querySelector(".projectsTab");
+
+
+projectBtn.addEventListener("click", () =>{
+    projectSection.style.display = "flex";
+})
+
+taskBtn.addEventListener("click", () =>{
+    taskSection.style.display = "flex";
+})
+
+
+titleForm.addEventListener("submit", event =>{
+    event.preventDefault();
+    const title = document.querySelector("#title").value;
+    const projects = document.createElement("p");
+    projects.textContent = `#${title}`;
+    projectsTabDiv.appendChild(projects);
+    document.querySelector("#title").value = '';
+    projectSection.style.display = "none";
+})
+
+projectCloseBtn.addEventListener("click", ()=>{
+    projectSection.style.display = "none";  
+})
+
+taskCloseBtn.addEventListener("click", ()=>{
+    taskSection.style.display = "none";
+})
 
 
 
-const home = createProject("Home");
 
-home.addToDo("Laundry","All to do works related to home", "10/09/2025", "medium");
-home.addToDo("Dishes","All to do works related to home", "10/09/2025", "medium");
 
-const office = createProject("Office");
+// const home = createProject("Home");
 
-office.addToDo("Papers","All to do works related to office", "10/09/2025", "medium");
-console.log(home);
-console.log(office);
+// home.addToDo("Laundry","All to do works related to home", "10/09/2025", "medium");
+// home.addToDo("Dishes","All to do works related to home", "10/09/2025", "medium");
+
+// const office = createProject("Office");
+
+// office.addToDo("Papers","All to do works related to office", "10/09/2025", "medium");
+// console.log(home);
+// console.log(office);
