51be08f144a1404ea4caa099f6664af53184e83a
Nimesh04 <nimeshb907@gmail.com>
2025-10-16 15:36:29 -0500

    New project creation & task adding to the project
    Every project now stores the task for it's self,
    without the conflict of task being added to the
    wrong project.

--- src/createProjects.js
+++ src/createProjects.js
@@ -1,13 +1,10 @@
 import { createToDo } from "./createToDo";
 
-export function createProject(title){
-    return{
-        title: title,
-        toDo: [],
-        addToDo: function(title, description, dueDate, priority){
-            const task = createToDo(title, description, dueDate, priority);
-            return this.toDo.push(task);
-        },
-        
-    }
+export function createProject(title, uuid){
+    const id = uuid;
+    const projectName = title;
+    let toDo = [];
+    const tasks = () => {return toDo}
+    const addToDo = (title, description, dueDate, priority) => toDo.push(createToDo(title, description, dueDate, priority));
+    return { id, projectName, tasks, addToDo};
 }
--- src/index.js
+++ src/index.js
@@ -10,8 +10,12 @@ const taskSection = document.querySelector(".taskSection");
 const projectCloseBtn = document.querySelector(".projectSection-heading > svg");
 const taskCloseBtn = document.querySelector(".taskSection-heading > svg");
 const titleForm = document.querySelector(".titleForm");
+const taskForm = document.querySelector(".taskForm");
 const projectsTabDiv = document.querySelector(".projectsTab");
+const taskSectionDiv = document.querySelector(".task-section");
 
+let currentProject = '';
+let projectsArr = [];
 
 projectBtn.addEventListener("click", () =>{
     projectSection.style.display = "flex";
@@ -25,11 +29,27 @@ taskBtn.addEventListener("click", () =>{
 titleForm.addEventListener("submit", event =>{
     event.preventDefault();
     const title = document.querySelector("#title").value;
+    const uuid = crypto.randomUUID();
+    const project = createProject(title, uuid);
+    projectsArr.push(project);
     const projects = document.createElement("p");
+    projects.classList.add("projects");
+    projects.dataset.id = `${uuid}`;
     projects.textContent = `#${title}`;
     projectsTabDiv.appendChild(projects);
     document.querySelector("#title").value = '';
     projectSection.style.display = "none";
+    // console.log("Project name:",project.projectName, projectsArr);
+})
+
+taskForm.addEventListener("submit", event =>{
+    event.preventDefault();
+    const formData = new FormData(taskForm);
+    const dataObject = Object.fromEntries(formData.entries());
+    currentProject.addToDo(dataObject.title, dataObject.description, dataObject.dueDate, dataObject.Priority );
+    taskForm.reset();
+    taskSection.style.display = "none";
+    checkTask();
 })
 
 projectCloseBtn.addEventListener("click", ()=>{
@@ -40,17 +60,31 @@ taskCloseBtn.addEventListener("click", ()=>{
     taskSection.style.display = "none";
 })
 
+// Capturing all the clicks that are done to change the projects and assigning the clicked one
+// so that the task can be added to the appropriate project.
+
+projectsTabDiv.addEventListener("click", (event) =>{
+    if(event.target.matches(".projects")){
+        const carry = projectsArr.filter(project => project.id == event.target.dataset.id);
+        currentProject = carry[0];
+    }
+})
 
 
-
-
-// const home = createProject("Home");
-
-// home.addToDo("Laundry","All to do works related to home", "10/09/2025", "medium");
-// home.addToDo("Dishes","All to do works related to home", "10/09/2025", "medium");
-
-// const office = createProject("Office");
-
-// office.addToDo("Papers","All to do works related to office", "10/09/2025", "medium");
-// console.log(home);
-// console.log(office);
+function checkTask(){
+    const taskList = currentProject.tasks();
+    console.log("taskList:", taskList);
+    const taskDiv = document.createElement("div");
+    taskDiv.classList.add("task-list");
+    taskList.forEach(element => {
+        console.log("element:", element);
+        // {title: 'fsadf', description: 'fsdafa', dueDate: '2025-10-16',
+        //  priority: 'High', completed: false}
+        taskDiv.innerHTML = 
+        `<p>Title : <span>${element.title}</span>  Due Date: <span>${element.dueDate}</p>
+        <p> Description:</p>
+        <p>${element.description}</p>
+        <input type="checkbox" name="completed" id="completed>`;
+        taskSectionDiv.appendChild(taskDiv);
+    });
+}
